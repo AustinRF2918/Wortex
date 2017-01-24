5752f8a178d391a2dba3fd4363b3a1cc93dcbfeb
Austin <austinrf2918@yahoo.com>
2017-01-23 21:42:45 -0500

    More  renames.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from argparse import ArgumentParser
-import session.session_management as sm
+from soft_retrieval.metadata_scraping import attempt_request, build_site_data
 from colorama import Style
 
 parser = ArgumentParser(description="Development framework for automating maintainence of small projects.")
@@ -9,6 +9,6 @@ parser.add_argument('--d', help='Download and authenticate a session.')
 args = parser.parse_args()
 
 if (args.d != None):
-    req = sm.attempt_request(args.d)
-    data = sm.build_site_data(req)
-    print(data)
+    req = attempt_request(args.d)
+    data = build_site_data(req)
+    print("Data that was found: {}".format(data))
